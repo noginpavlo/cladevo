0ab26c465f50b18e792f15020812d591b26cb00c
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-02-11 17:11:53 +0100

    Add one more template. Input. Male it worl by removing url_for f(x) form flask.

--- cladevo/tree_builder/urls.py
+++ cladevo/tree_builder/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("parameters/", views.parameters, name="parameters"),
+    path("sequence_input/", views.sequence_input, name="sequence_input"),
 ]
--- cladevo/tree_builder/views.py
+++ cladevo/tree_builder/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def parameters(request):
     return render(request, "tree_builder/parameters.html")
+
+def sequence_input(request):
+    return render(request, "tree_builder/input.html")
