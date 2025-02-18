fce704cd4800bd36e90012ab4f436a2565a57ec0
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-02-18 19:01:40 +0100

    Made file upload work. It builds trees. Still need to check if everything works as intended.

--- cladevo/tree_builder/urls.py
+++ cladevo/tree_builder/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import submit_sequences, show_result
+from .views import submit_sequences, show_result, submit_file
 
 urlpatterns = [
     path("parameters/", views.parameters, name="parameters"),
     path("sequence_input/", views.sequence_input, name="sequence_input"),
     path("submit_sequences/", submit_sequences, name="submit_sequences"),
     path("result/<str:unique_id>/", show_result, name="show_result"),
+    path("submit_file/", submit_file, name="submit_file"),
 ]
--- cladevo/tree_builder/views.py
+++ cladevo/tree_builder/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import json
+import os
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt  # Temporary (Not recommended for production)
 from tree_builder.services.calculator_upgma import TreeBuilder
@@ -44,6 +45,49 @@ def submit_sequences(request):
 
     else:
         return JsonResponse({"error": "Invalid request method"}, status=405)
+
+
+@csrf_exempt  # Remove this when CSRF protection is added in frontend
+def submit_file(request):
+    if request.method == "POST" and request.FILES.get("file-upload"):
+        file = request.FILES["file-upload"]
+
+        # Save the uploaded file
+        upload_dir = "static/uploads"  # Directory to save files
+        os.makedirs(upload_dir, exist_ok=True)  # Ensure the directory exists
+        file_path = os.path.join(upload_dir, file.name)
+
+        with open(file_path, "wb+") as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+
+        # Check file extensions
+        if file.name.lower().endswith(".fasta"):
+            # Handle FASTA file
+            calculator.align_sequences(fasta_file=file_path)
+        elif file.name.lower().endswith((".xls", ".xlsx")):
+            # Handle Excel file
+            calculator.reformat(file_path)
+        else:
+            # For unsupported file types
+            return JsonResponse({"error": "Unsupported file type"}, status=400)
+
+        calculator.calculate_dissimilarity_matrix()
+        calculator.build_tree(method)
+        unique_id = calculator.visualize_tree()
+
+        if unique_id is None:
+            return JsonResponse({"error": "Tree visualization failed"}, status=400)
+
+        # Build the redirect URL
+        redirect_url = f"/tree_builder/result/{unique_id}/"
+
+        # Return the URL as JSON
+        return JsonResponse({"redirect_url": redirect_url})
+
+    return JsonResponse({"error": "Invalid request"}, status=400)
+
 
 def show_result(request, unique_id):
     print(f"Received unique_id: {unique_id}")
